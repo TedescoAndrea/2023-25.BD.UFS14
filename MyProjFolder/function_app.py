52ede93aad5ca4ca0185e4e0802da8caa0053603
TedescoAndrea <andrea.tedesco@itsrizzoli.it>
2024-10-28 13:59:01 +0000

    miao

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -5,7 +5,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.Anonymous)
+@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
