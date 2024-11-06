b33ef36bfaed0aa2f6f4b724b730d22922157560
TedescoAndrea <andrea.tedesco@itsrizzoli.it>
2024-11-06 15:38:41 +0000

    fix

--- test_snapshot.py
+++ test_snapshot.py
@@ -4,4 +4,4 @@ def coso():
             
 def test_function_output_with_snapshot(snapshot):
     snapshot.snapshot_dir = 'snapshots'  # This line is optional.
-    snapshot.assert_match(foo('function input'), 'foo_output.txt')
+    snapshot.assert_match(('function input'), 'foo_output.txt')
