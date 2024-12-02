2a99447a99a2820f8c3a585411358aedb4627e4b
TedescoAndrea <andrea.tedesco@itsrizzoli.it>
2024-12-02 13:21:09 +0000

    fix

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -6,7 +6,7 @@ from .animal_detection import detect_animal_sound  # Importa la funzione
 
 app = func.FunctionApp()
 
-@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.anonymous)
+@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.Anonymous)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
